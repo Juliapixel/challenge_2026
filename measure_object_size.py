e34c1fd34780f3f2ab91f834df4fa90069d9d117
Raquel <raquel.oliamaral@gmail.com>
2026-08-17 21:29:00 -0300

    Adiciona função para identificar ID da estaca

--- measure_object_size.py
+++ measure_object_size.py
@@ -5,7 +5,7 @@ import cv2
 # marker_image = cv2.aruco.generateImageMarker(dicionario, 3, 200)
 # cv2.imwrite("marker3.png", marker_image)
 
-input_image = cv2.imread("teste.webp")
+input_image = cv2.imread("marcadores5.png")
 
 # Identificando marcadores
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
@@ -35,3 +35,28 @@ elif altura_grama > 15:
     print (" 🟡 Altura entre 15 e 30 cm")
 else:
     print (" 🟢 Altura menor que 15 cm")
+
+ # Função para saber o ID da estaca
+ # O ID da estaca é o ID do primeiro ArUco (de cima para baixo)
+def id_estaca (output_image, dictionary):
+    # Se nenhum ID for identificado
+    if marker_ids is None or len(marker_ids) == 0:
+        return None
+
+    marcadores = []
+
+    for i, corner in enumerate(marker_corners):
+        pontos = corner[0] # estamos alcançando as vertices de cada ArUco
+        centro_altura = pontos[:,1].mean() 
+        marcadores.append((int(marker_ids[i]), centro_altura))
+
+    marcadores.sort(key=lambda m: m[1])
+    primeiro_id = marcadores[0][0]
+
+    return primeiro_id
+
+print(f'ID da estaca: {id_estaca(output_image, dictionary)}')
+
+
+
+
