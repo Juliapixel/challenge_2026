e77dec144b9086c644e68c2957fbcc5363d1f8d6
Eduardoui <eduardobacelar2007@gmail.com>
2026-08-29 21:13:33 -0300

    integração visão computacional com FastAPI

--- measure_object_size.py
+++ measure_object_size.py
@@ -20,37 +20,42 @@ def id_estaca (marker_corners, marker_ids):
 
 
 def altura_grama(marker_ids):
-    ALTURA_MARCADOR_CM = 5.0  # Supondo que cada marcador tem 5 cm de altura
-    MARCADORES_POR_ESTACA = 4  # Cada estaca tem 4 marcadores
+    ALTURA_MARCADOR_CM = 10.0  # Supondo que cada marcador tem 10 cm de altura
+   # MARCADORES_POR_ESTACA = 4  # Cada estaca tem 4 marcadores
 
     if marker_ids is not None:
-        altura_grama = MARCADORES_POR_ESTACA * ALTURA_MARCADOR_CM - (ALTURA_MARCADOR_CM * len(marker_ids))
+        return len(marker_ids) * ALTURA_MARCADOR_CM
+        #altura_grama = MARCADORES_POR_ESTACA * ALTURA_MARCADOR_CM - (ALTURA_MARCADOR_CM * len(marker_ids))
     else:
-        altura_grama = 0.0
+        #altura_grama = 0.0
+        return 0.0
 
     return altura_grama
 
+def verificar_poda(marker_ids):
+    if marker_ids is None:
+        return False
 
-input_image = cv2.imread("teste.webp")
+    return len(marker_ids) >= 3 # Se houver 3 ou mais marcadores, a grama precisa ser podada
 
-# Identificando marcadores
-dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-parameters = cv2.aruco.DetectorParameters()
+# realizar a análise da imagem e retornar os resultados
+def analisar_imagem(input_image):
 
-detector = cv2.aruco.ArucoDetector(dictionary, parameters)
-marker_corners, marker_ids, rejected_candidates = detector.detectMarkers(input_image)
+    # identificando marcadores
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    parameters = cv2.aruco.DetectorParameters()
 
-# Mostrando
-output_image = input_image.copy()
-cv2.aruco.drawDetectedMarkers(output_image, marker_corners, marker_ids)
-cv2.namedWindow("contornos", cv2.WINDOW_NORMAL)
-cv2.imshow("contornos", output_image)
-cv2.waitKey(0)
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+    marker_corners, marker_ids, rejected_candidates = detector.detectMarkers(input_image)
 
+    # Obtendo os resultados da análise
+    estaca = id_estaca(marker_corners, marker_ids)
+    altura = altura_grama(marker_ids)
+    poda = verificar_poda(marker_ids),
 
-
-print(f'ID da estaca: {id_estaca(marker_corners, marker_ids)}')
-print(f'Altura da grama: {altura_grama(marker_ids)} cm')
-
-
-
+    return {
+        "id_estaca": estaca,
+        "altura_grama": altura,
+        "marcadores_detectados": len(marker_ids) if marker_ids is not None else 0,
+        "necessita_poda": poda
+    }
